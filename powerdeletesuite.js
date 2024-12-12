62774b18fd35ce7322c52d42f573e96ed51427c3
Grosan Flaviu Gheorghe <fgheorghe@grosan.co.uk>
2024-12-12 10:29:59 +0000

    Update powerdeletesuite.js

--- powerdeletesuite.js
+++ powerdeletesuite.js
@@ -909,7 +909,7 @@ var pd = {
           pd.task.after = pd.task.items[0].data.name;
           pd.actions.children.handleSingle();
         }
-      }, 1000);
+      }, 2000);
     },
     edit: function (item) {
       setTimeout(() => {
